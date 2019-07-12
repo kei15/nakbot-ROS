de888fcd3547535d619fbd509035ca1545615968
kei15 <micomicomicotaro@yahoo.co.jp>
2019-07-12 19:19:52 +0900

    pushのテスト

--- src/face_recog/getXYZ.py
+++ src/face_recog/getXYZ.py
@@ -21,24 +21,17 @@ def callback(data):
         cloud_points.append(p[2])
         clp0.append(p[0])
         clp1.append(p[1])
-        # print ("0==========" + str(p[0]))
-        # print ("1==========" + str(p[1]))
-        # print ("2==========" + str(p[2]))
 
-    # print ("2==========" + str(cloud_points))
     x_points = numpy.array(clp0, dtype=numpy.float32)
     y_points = numpy.array(clp1, dtype=numpy.float32)
     x = x_points.reshape(resolution)
     y = y_points.reshape(resolution)
-
-    # print ("y==========" + str(y[data.height/2 , data.width/2]))
 
     z_points = numpy.array(cloud_points, dtype=numpy.float32)
     z = z_points.reshape(resolution)
 
     print ("x: " + str(x[data.height/2 , data.width/4]) + ", y: " + str(y[data.height/2 , data.width/4]) + ", z: " + str(z[data.height/2 , data.width/4]))
     print ("x0: " + str(x[data.height/2 , data.width/2]) + ", y0: " + str(y[data.height/2 , data.width/2]) + ", z0: " + str(z[data.height/2 , data.width/2]))
-    # print z[data.height/2 , data.width/2]
 
 def listener():
     rospy.init_node('listener', anonymous=True)
